948161fa36ddc0d79ea9c25f7ebfe0a900ae74fb
Daniel Livingston <daniel.livingston@amd.com>
2022-06-03 19:09:54 -0400

    Fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,16 +1,5 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    echo status: interactive
-    fish_add_path ~/.local/bin
-end
-
 if status --is-login
-    #set -gx PATH $PATH ~/.local/bin
-    echo status: login
     fish_add_path ~/.local/bin
-end
-
-function on_exit --on-event fish_exit
-    echo status: exiting
+    echo status: login
 end
 
